4e588ba74d1b916a501570638f29d14c05bc6e2a
Shlomi Fish <shlomif@iglu.org.il>
2011-10-29 16:50:03 +0200

    Add some rudimentary POD.
    To lib/Array/Columnize.pm.

--- lib/Array/Columnize.pm
+++ lib/Array/Columnize.pm
@@ -45,3 +45,72 @@ if (__FILE__ eq $0 ) {
 }
 
 1;
+
+=encoding utf8
+
+=head1 NAME
+
+Array::Columnize - arrange data in columns.
+
+=head1 SYNOPSIS
+
+    use Array::Columnize;
+
+    # TODO - FILL IN.
+
+=head1 DESCRIPTION
+
+B<TODO> - FILL IN.
+
+=head1 AUTHOR
+
+Rocky Bernstein, C<< rocky@cpan.org >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-array-columnize at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Array-Columnize>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Array::Columnize
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Array-Columnize>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Array-Columnize>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Array-Columnize>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Array-Columnize>
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (c) 2011 Rocky Bernstein.
+
+=head2 LICENSE
+
+Same terms as Perl.
+
+=cut
+
+1;
+
