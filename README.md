50f631292ce0341fa53b37e32753ea1569d64400
rocky <rocky@cpan.org>
2011-10-29 11:29:56 -0400

    Columnize.pm: fill in POD from README.md README.md: correct a couple of typos Changes: Did I catch everything Shlomi?

--- README.md
+++ README.md
@@ -10,8 +10,11 @@ Setup
 -----
 
     use Array::Columnize;
-    print columnize(['a','b','c','d'], {displaywidth=>4});
 
+Simple data example 
+-------------------
+
+    print columnize(['a','b','c','d'], {displaywidth=>4}), "\n";
 produces:
 
     a  c
@@ -31,15 +34,15 @@ produces:
 
 while:
 
-    print columnize($data_ref, {ljust = 0, arrange_verticle = 0}) ;
+    print columnize($data_ref, {ljust = 0, arrange_vertical = 0}) ;
 
 produces:
 
-    80  83  86  89  92  95   98  101  104  107  110  113  116  119
-    81  84  87  90  93  96   99  102  105  108  111  114  117  120
-    82  85  88  91  94  97  100  103  106  109  112  115  118
-
-(NOTE: what's the difference)?
+     80   81   82   83   84   85   86   87   88   89
+     90   91   92   93   94   95   96   97   98   99
+    100  101  102  103  104  105  106  107  108  109
+    110  111  112  113  114  115  116  117  118  119
+    120
 
 With String data
 ----------------
--- lib/Array/Columnize.pm
+++ lib/Array/Columnize.pm
@@ -50,17 +50,74 @@ if (__FILE__ eq $0 ) {
 
 =head1 NAME
 
-Array::Columnize - arrange data in columns.
+Array::Columnize - arrange list data in columns.
 
 =head1 SYNOPSIS
 
     use Array::Columnize;
-
-    # TODO - FILL IN.
+    print columnize($array_ref, $optional_hash_or_hash_ref), "\n";
 
 =head1 DESCRIPTION
 
-B<TODO> - FILL IN.
+In showing long lists, sometimes one would prefer to see the values
+arranged and aligned in columns. Some examples include listing methods of
+an object, listing debugger commands, or showing a numeric array with data
+aligned.
+
+=head1 EXAMPLES
+
+=head2 Simple data example 
+
+    print columnize(['a','b','c','d'], {displaywidth=>4}), "\n";
+
+produces:
+
+    a  c
+    b  d
+
+=head2 With numeric data
+
+    my $data_ref = [80..120];
+    print columnize($data_ref, {ljust = 0}) ;
+
+produces:
+
+    80  83  86  89  92  95   98  101  104  107  110  113  116  119
+    81  84  87  90  93  96   99  102  105  108  111  114  117  120
+    82  85  88  91  94  97  100  103  106  109  112  115  118
+
+while:
+
+    print columnize($data_ref, {ljust = 0, arrange_vertical = 0}) ;
+
+produces:
+
+     80   81   82   83   84   85   86   87   88   89
+     90   91   92   93   94   95   96   97   98   99
+    100  101  102  103  104  105  106  107  108  109
+    110  111  112  113  114  115  116  117  118  119
+    120
+
+=head2 With String data
+
+    @ary = qw(bibrons golden madascar leopard mourning suras tokay);
+    print columnize(\@ary, {displaywidth => 18});
+
+produces: 
+
+    bibrons   mourning
+    golden    suras   
+    madascar  tokay   
+    leopard 
+
+    puts columnize \@ary, {displaywidth => 18, colsep => ' | '};
+
+produces:
+
+    bibrons  | mourning
+    golden   | suras   
+    madascar | tokay   
+    leopard 
 
 =head1 AUTHOR
 
